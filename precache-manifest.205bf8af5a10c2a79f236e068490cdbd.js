f1a10888e99837616198ed280a57455c667734bf
Robinkariuki <robinkaris7@gmail.com>
2020-06-14 13:01:45 +0300

    Updates

---
+++ precache-manifest.205bf8af5a10c2a79f236e068490cdbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c84eb86ebdfba8248f08326b2bb5eaf",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "928c4657d01c48994740",
+    "url": "/weather/static/css/main.bac96978.chunk.css"
+  },
+  {
+    "revision": "394ed6084b70c0885b03",
+    "url": "/weather/static/js/2.217bc631.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.217bc631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "928c4657d01c48994740",
+    "url": "/weather/static/js/main.ab4ff3ee.chunk.js"
+  },
+  {
+    "revision": "b83e05f3b1e38480a3ef",
+    "url": "/weather/static/js/runtime-main.d16f45af.js"
+  }
+]);
